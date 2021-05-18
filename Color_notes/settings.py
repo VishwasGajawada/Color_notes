d9d56d1ef20a127a3281e728f56ae52135eb247f
VishwasGajawada <vishwasgajawada2002@gmail.com>
2021-05-18 13:54:59 +0530

    Added whitenoise storage

--- Color_notes/settings.py
+++ Color_notes/settings.py
@@ -137,3 +137,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
